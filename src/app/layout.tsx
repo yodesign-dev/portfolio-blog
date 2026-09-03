a986733cbbba8e8c1744df733cf6e7ae629ca8af
Binh Nguyen <BinDesigner@MacBook-Pro.local>
2026-09-03 14:19:41 +0700

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mulish, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 // 1. Khởi tạo font Mulish (hỗ trợ Tiếng Việt) và gán vào biến --font-geist-sans để map trúng file globals.css cũ
 const mulish = Mulish({
@@ -28,7 +29,7 @@ export default function RootLayout({
       lang="vi"
       className={`${mulish.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children} <Analytics /> </body>
     </html>
   );
 }
